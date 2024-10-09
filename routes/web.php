35199ab8545bb6b4257688a57ace4aa0f805f7b9
RizzCode10 <rakarizi9@gmail.com>
2024-10-09 13:33:12 +0700

    fortivy login & logout

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.auth-login');
 });
 
-Route::get('/dashboard', function () {
-    return view('pages.dashboard', ['type_menu' => 'dashboard']);
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard', ['type_menu' => 'home']);
+    })->name('home');
+
+    // Route::resource('users', UserController::class);
+    // Route::resource('companies', CompanyController::class);
+    // Route::resource('attendances', AttendanceController::class);
+    // Route::resource('permissions', PermissionController::class);
 });
 
-Route::get('/login', function () {
-    return view('pages.auth.auth-login', ['type_menu' => 'dashboard']);
-});
+// Route::get('/dashboard', function () {
+//     return view('pages.dashboard', ['type_menu' => 'dashboard']);
+// });
+
+
